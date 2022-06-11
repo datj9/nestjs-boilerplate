ab3b1a603c1636d16bba2efb099e5324ed5755c2
Dat Nguyen <tandat198@gmail.com>
2022-06-11 23:54:10 +0700

    add signin api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/CreateUserDto';
+import { GetUserDto } from './dtos/GetUserDto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,11 @@ export class AuthController {
   @Post('signup')
   signUp(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUp(createUserDto);
+  }
+
+  @Post('signin')
+  @HttpCode(200)
+  signIn(@Body() getUserDto: GetUserDto) {
+    return this.authService.signIn(getUserDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/CreateUserDto';
+import { GetUserDto } from './dtos/GetUserDto';
 import { User } from './entities/User';
 import Hashing from './hashing';
 
@@ -11,6 +12,11 @@ export class AuthService {
 
   async signUp(createUserDto: CreateUserDto) {
     try {
+      if (createUserDto.password !== createUserDto.confirmPassword) {
+        return {
+          code: 'password_does_not_match_confirm_password',
+        };
+      }
       const newUser = this.userRepo.create({
         ...createUserDto,
         password: await Hashing.hash(createUserDto.password),
@@ -26,5 +32,23 @@ export class AuthService {
         return { message: 'User already exists', code: 'duplicated_user' };
       }
     }
+  }
+
+  async signIn(getUserDto: GetUserDto) {
+    const user = await this.userRepo.findOne({
+      where: { email: getUserDto.email },
+      select: ['id', 'email', 'firstName', 'lastName', 'password'],
+    });
+
+    if (!user) {
+      return { code: 'user_not_found' };
+    }
+
+    const isValid = await Hashing.compare(getUserDto.password, user.password);
+    if (!isValid) {
+      return { code: 'invalid_password' };
+    }
+    delete user.password;
+    return user;
   }
 }
