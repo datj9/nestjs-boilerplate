7f615338b600917e60e4526777a1a90649a70a64
Dat Nguyen <tandat198@gmail.com>
2022-06-26 07:37:27 +0700

    config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,10 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.development.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
